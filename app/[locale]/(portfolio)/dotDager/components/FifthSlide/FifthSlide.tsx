96db799971ed7fc6f3dcdcdde0970edc5e975c46
AlejandroLunaDev <alejandrolunadev@gmail.com>
2024-11-21 12:39:49 -0300

    Feat: 🏗️❕ Contact in progress

--- app/[locale]/(portfolio)/dotDager/components/FifthSlide/FifthSlide.tsx
+++ app/[locale]/(portfolio)/dotDager/components/FifthSlide/FifthSlide.tsx
@@ -1,57 +1,13 @@
 'use client';
 
-import { useState, useEffect } from 'react';
-import Cookies from 'js-cookie';
-import ButtonAnimate from '../../../../../shared/components/ButtonAnimate/ButtonAnimate';
-import styles from './FifthSlide.module.scss';
-import { useTranslations } from 'next-intl';
+import FormContact from './components/FormContact';
 
 export default function FifthSlide() {
-  const t = useTranslations('FifthSlide');
-  const [locale, setLocale] = useState(Cookies.get('NEXT_LOCALE') || 'en');
-
-  useEffect(() => {
-    const updateLocale = () => {
-      const detectedLocale = Cookies.get('NEXT_LOCALE') || 'en';
-      setLocale(detectedLocale);
-    };
-
-    window.addEventListener('focus', updateLocale);
-
-    return () => {
-      window.removeEventListener('focus', updateLocale);
-    };
-  }, []);
-
-  const cvLink = `${window.location.origin}/files/${locale}-CV.pdf`;
 
   return (
-    <div className={styles.Links}>
-      <div className={styles.proyectos}>
-        <header>
-          <h1>{t('links.projects.title')}</h1>
-          <p>{t('links.projects.description')}</p>
-        </header>
-        <ButtonAnimate
-          href="/projects"
-          hoverText={t('links.projects.button.hoverText')}
-        >
-          {t('links.projects.button.text')}
-        </ButtonAnimate>
-      </div>
-      <div className={styles.curriculum}>
-        <header>
-          <h1>{t('links.cv.title')}</h1>
-          <p>{t('links.cv.description')}</p>
-        </header>
-        <ButtonAnimate
-          href={cvLink}
-          download
-          hoverText={t('links.cv.button.hoverText')}
-        >
-          {t('links.cv.button.text')}
-        </ButtonAnimate>
-      </div>
-    </div>
-  );
+
+    <section>
+    <FormContact />
+    </section>
+  )
 }
--- app/[locale]/(portfolio)/dotDager/components/FifthSlide/components/FormContact.tsx
+++ app/[locale]/(portfolio)/dotDager/components/FifthSlide/components/FormContact.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 'use client';
 import { useState, ChangeEvent, FormEvent } from 'react';
-import { FaGithub, FaLinkedin } from 'react-icons/fa';
+import { FaGithub, FaLinkedin, FaYoutube, FaDiscord } from 'react-icons/fa';
 import styles from './FormContact.module.scss';
 import Image from 'next/image';
 import Link from 'next/link';
@@ -23,15 +23,11 @@ export default function FormContact() {
   const [submitted, setSubmitted] = useState<boolean>(false);
   const [error, setError] = useState<boolean>(false);
 
-
-
-  // Handle form field changes
   const handleChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
     const { name, value } = e.target;
     setFormData({ ...formData, [name]: value });
   };
 
-  // Handle form submission
   const handleSubmit = async (e: FormEvent) => {
     e.preventDefault();
     try {
@@ -54,7 +50,7 @@ export default function FormContact() {
     }
   };
 
-  const t= useTranslations('Contact'); 
+  const t = useTranslations('Contact');
 
   return (
     <div className={styles.formContainer}>
@@ -68,59 +64,69 @@ export default function FormContact() {
         {!submitted ? (
           <form onSubmit={handleSubmit}>
             <div>
-              <label htmlFor='name'>{t('form.name')}</label>
+              <label htmlFor="name">{t('form.name')}</label>
               <input
-                type='text'
-                id='name'
-                name='name'
+                type="text"
+                id="name"
+                name="name"
                 value={formData.name}
                 onChange={handleChange}
                 required
               />
             </div>
             <div>
-              <label htmlFor='email'>{t('form.email')}</label>
+              <label htmlFor="email">{t('form.email')}</label>
               <input
-                type='email'
-                id='email'
-                name='email'
+                type="email"
+                id="email"
+                name="email"
                 value={formData.email}
                 onChange={handleChange}
                 required
               />
             </div>
             <div>
-              <label htmlFor='message'>{t('form.message')}</label>
+              <label htmlFor="message">{t('form.message')}</label>
               <textarea
-                id='message'
-                name='message'
+                id="message"
+                name="message"
                 value={formData.message}
                 onChange={handleChange}
                 required
               ></textarea>
             </div>
-            <ButtonAnimate type='submit' hoverText={t('form.button.hoverText')}>
+            <ButtonAnimate type="submit" hoverText={t('form.button.hoverText')}>
               {t('form.button.text')}
             </ButtonAnimate>
-            {error && (
-              <p className={styles.error}>
-                {t('form.error')}
-              </p>
-            )}
+            {error && <p className={styles.error}>{t('form.error')}</p>}
             <div className={styles.iconsform}>
               <Link
-                href='https://github.com/AlejandroLunaDev'
-                target='_blank'
-                rel='noopener noreferrer'
+                href="https://github.com/AlejandroLunaDev"
+                target="_blank"
+                rel="noopener noreferrer"
               >
                 <FaGithub size={30} style={{ color: '#43efff' }} />
               </Link>
               <Link
-                href='https://www.linkedin.com/in/alejandro-luna-dev'
-                target='_blank'
-                rel='noopener noreferrer'
+                href="https://www.linkedin.com/in/alejandro-luna-dev"
+                target="_blank"
+                rel="noopener noreferrer"
               >
                 <FaLinkedin size={30} style={{ color: '#43efff' }} />
+              </Link>
+              <Link
+                href="https://www.youtube.com/channel/your-channel-id"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaYoutube size={30} style={{ color: '#FF0000' }} />
+              </Link>
+              <Link
+                href="https://discord.gg/your-invite-link"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaDiscord size={30} style={{ color: '#7289DA' }} />
               </Link>
             </div>
           </form>
@@ -132,21 +138,38 @@ export default function FormContact() {
         )}
       </div>
       <div className={styles.connect}>
-        <Image width={500} height={500} src='/svgs/LogoAle3.svg' alt='logo' />
+        <div>
+          <p>{t('title')}</p>
+        </div>
+        <Image width={500} height={500} src="/image/despuesdelas12.png" alt="logo" />
         <div className={styles.icons}>
           <Link
-            href='https://github.com/AlejandroLunaDev'
-            target='_blank'
-            rel='noopener noreferrer'
+            href="https://github.com/AlejandroLunaDev"
+            target="_blank"
+            rel="noopener noreferrer"
           >
             <FaGithub size={40} style={{ color: '#ffff' }} />
           </Link>
           <Link
-            href='https://www.linkedin.com/in/alejandro-luna-dev'
-            target='_blank'
-            rel='noopener noreferrer'
+            href="https://www.linkedin.com/in/alejandro-luna-dev"
+            target="_blank"
+            rel="noopener noreferrer"
           >
             <FaLinkedin size={40} style={{ color: '#ffff' }} />
+          </Link>
+          <Link
+            href="https://www.youtube.com/channel/your-channel-id"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaYoutube size={40}  />
+          </Link>
+          <Link
+            href="https://discord.gg/your-invite-link"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaDiscord size={40}  />
           </Link>
         </div>
       </div>
